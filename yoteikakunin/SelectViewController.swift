240ae89e5fb8713e518a72649b9ebdd05592bd2e
reina <reina@MacBook-Air.local>
2015-05-04 17:04:31 +0900

    Initial commit

---
+++ yoteikakunin/SelectViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SelectViewController.swift
+//  yoteikakunin
+//
+//  Created by reina on 2015/05/04.
+//  Copyright (c) 2015年 srrn. All rights reserved.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func plus(){
+        let alert = UIAlertView()
+        alert.title = "警告"
+        alert.message = "送信しますか？"
+        alert.addButtonWithTitle("送信")
+        alert.addButtonWithTitle("キャンセル")
+        alert.show()
+        
+    }
+
+}
